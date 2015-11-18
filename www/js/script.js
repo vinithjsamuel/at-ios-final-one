6c45433f3236dcb001babf12897520305ecfd304
Vinith J Samuel <vinithjsamuel@gmail.com>
2015-11-18 15:53:36 +0400

    App notifications Updated

--- www/js/script.js
+++ www/js/script.js
@@ -98,3 +98,97 @@ $(document).ready(function(e) {
 	})
 	
 });
+
+
+
+var pushapp = {
+	// Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+    	document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        pushapp.receivedEvent('deviceready');
+    },
+    tokenHandler:function(msg) {
+    	atsavetodb(msg);
+    	console.log("Token Handler " + msg);
+    	alert(JSON.stringify(msg));
+    },
+    errorHandler:function(error) {
+        console.log("Error Handler  " + error);
+    },
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        alert('Success! Result = '+result);
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(this.tokenHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"pushapp.onNotificationAPN"});
+    },
+    // iOS
+    onNotificationAPN: function(event) {
+        var pushNotification = window.plugins.pushNotification;
+        console.log("Received a notification! " + event.alert);
+        console.log("event sound " + event.sound);
+        console.log("event badge " + event.badge);
+        console.log("event " + event);
+        if (event.alert) {
+            navigator.notification.alert(event.alert);
+        }
+        if (event.badge) {
+            console.log("Set badge on  " + pushNotification);
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, event.badge);
+        }
+        if (event.sound) {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
+    },
+    // Android
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    // Your GCM push server needs to know the regID before it can push to this device
+                    // here is where you might want to send it the regID for later use.
+                    alert('registration id = '+e.regid);
+                }
+            break;
+
+            case 'message':
+              // this is the actual push notification. its format depends on the data model
+              // of the intermediary push server which must also be reflected in GCMIntentService.java
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    }
+
+};
+pushapp.initialize();
+function atsavetodb(regid){
+    $.post("http://aesthetictoday.com/ajax/android/pushnotification.php",{ios_insert_regid: regid}, function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+}
